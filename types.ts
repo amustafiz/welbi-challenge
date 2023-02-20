64d98154a9121a62b1cac5b594e1f1d22c7983dc
amustafiz <munikamustafiz89@gmail.com>
2023-02-20 16:31:50 -0500

    Add types for models

---
+++ types.ts
@@ -0,0 +1,54 @@
+type LevelOfCare = "INDEPENDENT" | "ASSISTED" | "MEMORY" | "LONGTERM";
+
+export interface Attendance {
+  programId: number;
+  residentId: number;
+  status: "Active" | "Passive" | "Declined" | "Undefined";
+}
+
+
+export interface Resident {
+  id: number;
+  name: string;
+  firstName: string;
+  lastName: string;
+  preferredName:null|string;
+  status:null|string;
+  room: string;
+  levelOfCare: LevelOfCare;
+  ambulation: "NOLIMITATIONS" | "CANE" | "WALKER" | "WHEELCHAIR";
+  birthDate: string;
+  moveInDate: string;
+  createdAt: string;
+  updatedAt: string;
+  applicantId: number;
+  attendance: Attendance[];
+}
+
+interface Recurrence {
+    byMonth:number;
+    interval:number;
+    frequency:string;
+    byMonthday:number;
+}
+
+export interface Program {
+  id: number;
+  parentId: null | number;
+  name: string;
+  location: string;
+  allDay: boolean;
+  start: string;
+  end: string;
+  tags: string[];
+  createdAt: string;
+  updatedAt: string;
+  dimension: string;
+  facilitators: string[];
+  levelOfCare: LevelOfCare[];
+  hobbies: string[];
+  recurrence: null | Recurrence;
+  isRepeated: boolean;
+  applicantId: null | number;
+  attendance: Attendance[];
+}
